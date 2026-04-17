685e7b891a34219a1b83e1330021de1b96380234
13692277450 <13692277450@139.com>
2026-04-17 20:44:03 +0800

    add

---
+++ student.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include "Identity.h"
+using namespace std;
+
+class Student : public Identity
+{
+
+public:
+    Student();
+    Student(int id, string name, string pwd);
+    virtual void operMenu();
+    void applyOrder();
+	void showMyOrder();
+	void showAllOrder();
+    void cancelOrder();
+
+    int m_Id;
+
+};
